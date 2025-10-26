111535f3407c95584913f5baef032ba91c00360b
Tolben <vitek.pasek@gmail.com>
2025-10-26 14:01:48 +0100

    chore(codegen): add/update generated PHP (Module, Joins, Repository, Mapper, Exceptions) from schema-map

--- schema/030_foreign_keys.postgres.sql
+++ schema/030_foreign_keys.postgres.sql
@@ -1,4 +1,4 @@
--- Auto-generated from schema-map-postgres.psd1 (map@mtime:2025-10-24T09:46:38Z)
+-- Auto-generated from schema-map-postgres.psd1 (map@38d5403)
 -- engine: postgres
 -- table:  invoices
 ALTER TABLE invoices ADD CONSTRAINT fk_invoices_order FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE SET NULL;
