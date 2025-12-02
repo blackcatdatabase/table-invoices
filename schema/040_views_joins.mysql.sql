913d59f2b2e109295fba55e568eede3ff1a488db
Vito416 <vito416@protonmail.com>
2025-12-02 19:26:48 +0100

    Sync submodule state after YAML views + upsert/coverage fixes

--- schema/040_views_joins.mysql.sql
+++ schema/040_views_joins.mysql.sql
@@ -1,7 +1,7 @@
--- Auto-generated from joins-mysql.psd1 (map@mtime:2025-11-27T17:49:37Z)
+-- Auto-generated from joins-mysql.yaml (map@94ebe6c)
 -- engine: mysql
 -- view:   invoices_with_items
--- Invoices with item count and summed lines
+
 CREATE OR REPLACE ALGORITHM=TEMPTABLE SQL SECURITY INVOKER VIEW vw_invoices_with_items AS
 SELECT
   i.id,
