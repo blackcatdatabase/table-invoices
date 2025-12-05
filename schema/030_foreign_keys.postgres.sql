041c7a71e8cf377947c51c83c5d379f408724de4
Vito416 <vito416@protonmail.com>
2025-12-05 16:32:37 +0100

    Regenerate PHP from schema

--- schema/030_foreign_keys.postgres.sql
+++ schema/030_foreign_keys.postgres.sql
@@ -4,4 +4,4 @@
 
 ALTER TABLE invoices ADD CONSTRAINT fk_invoices_tenant FOREIGN KEY (tenant_id) REFERENCES tenants(id) ON DELETE RESTRICT;
 
-ALTER TABLE invoices ADD CONSTRAINT fk_invoices_order FOREIGN KEY (tenant_id, order_id) REFERENCES orders(tenant_id, id) ON DELETE SET NULL;
+ALTER TABLE invoices ADD CONSTRAINT fk_invoices_order FOREIGN KEY (tenant_id, order_id) REFERENCES orders(tenant_id, id) ON DELETE RESTRICT;
